3891ce51f12eeb592da1da4d6470295905463248
Stephen Baynham <sdbaynham@gmail.com>
2022-03-28 07:57:01 -0700

    khr_relaxed_block_layout

---
+++ extensions/khr_relaxed_block_layout/const.go
@@ -0,0 +1,11 @@
+package khr_relaxed_block_layout
+
+/*
+#include <stdlib.h>
+#include "../vulkan/vulkan.h"
+*/
+import "C"
+
+const (
+	ExtensionName string = C.VK_KHR_RELAXED_BLOCK_LAYOUT_EXTENSION_NAME
+)
