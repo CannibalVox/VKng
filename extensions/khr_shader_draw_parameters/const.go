a4c56181709af3990ede60a2757d6d8a7efc9752
Stephen Baynham <sdbaynham@gmail.com>
2022-03-28 07:59:02 -0700

    khr_shader_draw_parameters

---
+++ extensions/khr_shader_draw_parameters/const.go
@@ -0,0 +1,11 @@
+package khr_shader_draw_parameters
+
+/*
+#include <stdlib.h>
+#include "../vulkan/vulkan.h"
+*/
+import "C"
+
+const (
+	ExtensionName string = C.VK_KHR_SHADER_DRAW_PARAMETERS_EXTENSION_NAME
+)
