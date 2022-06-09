e5044216194f2f3d506d781327d4758132704203
Stephen Baynham <sdbaynham@gmail.com>
2022-06-08 17:14:34 -0700

    ext_shader_viewport_index_layer

---
+++ extensions/ext_shader_viewport_index_layer/const.go
@@ -0,0 +1,11 @@
+package ext_shader_viewport_index_layer
+
+/*
+#include <stdlib.h>
+#include "../vulkan/vulkan.h"
+*/
+import "C"
+
+const (
+	ExtensionName string = C.VK_EXT_SHADER_VIEWPORT_INDEX_LAYER_EXTENSION_NAME
+)
